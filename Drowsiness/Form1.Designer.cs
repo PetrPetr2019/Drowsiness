95e969bd443355046e143d4deb7829d6da4d8e29
PetrPetr <ferel2015@yandex.ru>
2019-09-20 12:58:44 +0300

    Add project files.

---
+++ Drowsiness/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Drowsiness
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Red;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(123, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(321, 10);
+            this.panel2.TabIndex = 12;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Red;
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Controls.Add(this.pictureBox1);
+            this.panel4.Location = new System.Drawing.Point(144, 7);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(72, 64);
+            this.panel4.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(2, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "EyeAlert";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(49, 36);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+           // this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Firebrick;
+            this.panel3.Location = new System.Drawing.Point(123, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(39, 38);
+            this.panel3.TabIndex = 13;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(123, 348);
+            this.panel1.TabIndex = 11;
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Image = global::Drowsiness.Properties.Resources.video;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button4.Location = new System.Drawing.Point(8, 178);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 57);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Launch";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Image = global::Drowsiness.Properties.Resources.pause;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button3.Location = new System.Drawing.Point(8, 105);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 57);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Stop ";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            this.panel5.AutoScroll = true;
+            this.panel5.BackColor = System.Drawing.Color.Red;
+            this.panel5.Location = new System.Drawing.Point(3, 31);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(10, 56);
+            this.panel5.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Image = global::Drowsiness.Properties.Resources.video;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.Location = new System.Drawing.Point(12, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 57);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Launch";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.BackColor = System.Drawing.Color.White;
+            this.imageBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox2.Location = new System.Drawing.Point(123, 71);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(322, 253);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+//            this.imageBox2.Click += new System.EventHandler(this.imageBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(444, 348);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.imageBox2);
+            this.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.6D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+           // this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Move);
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Drowsiness/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Tulpep.NotificationWindow;
+using System.Collections.Generic;
+using Drowsiness.Properties;
+
+namespace Drowsiness
+{
+    public partial class Form1 : Form
+    {
+//        private Timer timer = new Timer();
+//        private Capture camera;
+        private int _count;
+        private int _i;
+        private Image<Bgr, byte> _currentFrame;
+        private VideoCapture _camera;
+        private readonly CascadeClassifier _eye;
+        private readonly CascadeClassifier _faces;
+
+       public Form1()
+       {
+           _eye = new CascadeClassifier("haarcascade_eye.xml");
+           _faces = new CascadeClassifier("haarcascade_frontalface_default.xml");
+//            ProfFace = new CascadeClassifier("haarcascade_profileface.xml");
+            TopMost = true;
+           InitializeComponent();
+//           timer.Enabled = true;
+            timer2.Enabled = true;
+//            timer.Interval = 60000;
+//            timer.Tick += Timer_Tick;
+        }
+
+       private void Timer_Tick(object sender, EventArgs e)
+       {
+           if (DateTime.Now.TimeOfDay <= new TimeSpan(2)) return;
+           var pop = new PopupNotifier
+           {
+               Image = Resources.Look,
+               ContentText = "Attention!.",
+               BodyColor = Color.Green,
+               ContentColor = Color.White,
+               AnimationInterval = 3000,
+               AnimationDuration = 100,
+               ContentFont = new Font("Tahoma", 15F),
+               Delay = 4000,
+               BorderColor = Color.Yellow
+           };
+           pop.Popup();
+       }
+
+       static Dictionary<DateTime, string> signal; // Создал словарь
+/*
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+
+            if (DateTime.Now.TimeOfDay > new TimeSpan(2))
+            {
+
+                PopupNotifier p = new PopupNotifier();
+                p.Image = Properties.Resources.Look;
+                p.ContentText = "Пора сделать перерыв";
+                p.BodyColor = Color.Teal;
+                p.ContentColor = Color.White;
+                p.AnimationInterval = 1000;
+                p.AnimationDuration = 100;
+                p.ContentFont = new Font("Tahoma", 10F);
+                p.Delay = 2000;
+                p.BorderColor = Color.Yellow;
+                p.Popup();
+                //i = i + 1;
+            }
+
+        }
+*/
+
+        public void FormOpacity()
+        {
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            AllowTransparency = true;
+            BackColor = Color.AliceBlue; //цвет фона  
+            TransparencyKey = BackColor; //он же будет заменен на прозрачный цвет
+            panel1.BackColor = Color.Transparent;
+            BackColor = label1.BackColor = Color.AliceBlue;
+        }
+
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            _currentFrame = _camera.QueryFrame().ToImage<Bgr, byte>().Resize(640, 480, Inter.Cubic);
+            var faceDetect = _faces.DetectMultiScale(_currentFrame, 1.2, 10, new Size(10, 10), new Size(640, 480));
+            foreach (var f in faceDetect)
+            {
+                _currentFrame.Draw(f, new Bgr(Color.Blue), 2);
+
+
+                var eyeDetect = _eye.DetectMultiScale(_currentFrame,
+                    1.2,
+                    10,
+                    new Size(8,
+                        8),
+                    new Size(160,
+                        120));
+                if (eyeDetect.Length <= 0)
+                {
+                    var image = new Image<Gray, byte>(_currentFrame.Width, _currentFrame.Height, new Gray(0));
+                    _currentFrame.Canny(50, 20);
+                    imageBox2.Image = _currentFrame;
+                    _currentFrame.Draw(f, new Bgr(Color.Red), 5);
+                    _currentFrame.Draw("Close Eye", new Point(f.X, f.Y), FontFace.HersheyTriplex, 2.0,
+                        new Bgr(Color.White));
+//                    label2.Text = i++.ToString();
+
+
+                    Invoke((MethodInvoker) delegate
+                    {
+                        {
+                            var popup = new PopupNotifier
+                            {
+                                Image = Resources.Look,
+                                ContentText = "Attention!\nOften squinting",
+                                BodyColor = Color.Teal,
+                                ContentColor = Color.White,
+                                AnimationInterval = 1000,
+                                AnimationDuration = 100,
+                                ContentFont = new Font("Tahoma",
+                                10F),
+                                Delay = 2000,
+                                BorderColor = Color.Yellow
+                            };
+                            popup.Popup();
+                            _i += 1;
+
+                            if (_i <= 8) return;
+                            var pop = new PopupNotifier
+                            {
+                                Image = Resources.Look,
+                                ContentText = "Attention!\nEyes are tired.",
+                                BodyColor = Color.Green,
+                                ContentColor = Color.White,
+                                AnimationInterval = 3000,
+                                AnimationDuration = 100,
+                                ContentFont = new Font("Tahoma",
+                                    15F),
+                                Delay = 4000,
+                                BorderColor = Color.Yellow
+                            };
+                            _i = 0;
+
+                            pop.Popup();
+                        }
+                    });
+                    _count = 0;
+                }
+                else
+                {
+                    _count = _count + 1;
+                }
+
+                if (_count >= 20)
+                    _count = 0;
+                foreach (var eye in eyeDetect)
+                {
+                    eye.Inflate(-8,
+                        -8);
+                    _currentFrame.Draw(eye,
+                        new Bgr(Color.Blue),
+                        5);
+                }
+
+                _currentFrame.ROI = f;
+                FormOpacity();
+            }
+
+            _currentFrame.ROI = Rectangle.Empty;
+            imageBox2.Image = _currentFrame;
+        }
+
+        private void button2_Click(object sender,
+            EventArgs e)
+        {
+            Invoke((MethodInvoker) delegate
+            {
+                _camera = new VideoCapture();
+                Application.Idle += Application_Idle;
+                button2.Visible = true;
+//                label1.Visible = true;
+            });
+        }
+
+        /*
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private Point MouseHook;
+        private new void Move(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.Default;
+            if (e.Button != MouseButtons.Left) MouseHook = e.Location;
+            else
+            {
+                Location = new Point((Size)Location - (Size)MouseHook + (Size)e.Location);
+                Cursor = Cursors.Hand;
+            }
+
+        }
+*/
+
+
+    }
+   
+}
+
+
+
+   
+   
+
+
+           
+
+    
+
